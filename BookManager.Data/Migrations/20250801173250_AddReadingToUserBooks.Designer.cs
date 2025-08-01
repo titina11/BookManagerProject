883ae8da787c0d97aec815591dcf75545de0b3f1
tatyanagoranova <titina11@abv.bg>
2025-08-01 20:34:31 +0300

    Added migration with diary

--- BookManager.Data/Migrations/20250801173250_AddReadingToUserBooks.Designer.cs
+++ BookManager.Data/Migrations/20250801173250_AddReadingToUserBooks.Designer.cs
@@ -4,6 +4,7 @@ using BookManager.Web.Areas.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookManager.Data.Migrations
 {
     [DbContext(typeof(BookManagerDbContext))]
-    partial class BookManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250801173250_AddReadingToUserBooks")]
+    partial class AddReadingToUserBooks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookManager.Data/Migrations/20250801173250_AddReadingToUserBooks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReadingToUserBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "UserBooks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "UserBooks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rating",
+                table: "UserBooks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "UserBooks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "UserBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "UserBooks");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "UserBooks");
+        }
+    }
+}
